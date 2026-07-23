5db9056fe7d34720c017ad2a814c25fdb3a47b48
CJTorrez <javier.velasquez.78@gmail.com>
2026-07-22 19:23:22 -0600

    clase 1 mierrcoles

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Transactions;
+
+try
+{
+    Console.WriteLine("Ingrese primerr numero");
+    double n1 = double.Parse(Console.ReadLine() ?? "0");
+
+    Console.WriteLine("Ingrese segundo numero:");
+    double n2 = double.Parse(Console.ReadLine() ?? "0");
+
+    double division = n1 / n2;
+
+    Console.WriteLine("La diision es: " + division);
+    Console.WriteLine($"La fivision es: {division}"); //interrpellacion
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
